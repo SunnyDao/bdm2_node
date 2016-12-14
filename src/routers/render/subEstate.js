4df6bbf7cc890306f92dea596c8f82515482040a
tangxuyang <tangxuyang@lifang.com>
2016-12-14 14:57:10 +0800

    commit

--- src/routers/render/subEstate.js
+++ src/routers/render/subEstate.js
@@ -10,4 +10,23 @@ import proxyFactory from '../../core/proxyFactory';
 
 let router = express.Router();
 
+/**
+ * desc:小区子划分列表
+ */
+router.get("/list", function (req, res, next) {
+    let param = {
+        req: req,
+        matchJavascript: true,
+        matchStylesheet: false,
+        extraJavascripts: ["wktable"],
+        extraStylesheets: ["treeviewSelect", "wktable"]
+    }
+
+    let templateData = routerUtil.getTemplateBasicData(param);
+
+    Object.assign(templateData, { "title": "新BDM-小区子划分列表" });
+
+    return res.render("subEstate/list", templateData);
+})
+
 module.exports=router;
